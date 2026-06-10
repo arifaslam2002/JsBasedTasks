6b5475e1e02e75754c0143d6216b11b5c7a308f7
arifaslam2002 <aarifaslam2002@gmail.com>
2026-06-10 22:01:07 +0530

    Highest Salary

---
+++ salary.js
@@ -0,0 +1,10 @@
+let Salaryofemp1=70000;
+let Salaryofemp2=600000;
+let Salaryofemp3=30000;
+if(Salaryofemp1 >Salaryofemp2 && Salaryofemp1>Salaryofemp3){
+    console.log("Employee 1 has highest Salary Among other Employees")
+}else if (Salaryofemp2>Salaryofemp1 && Salaryofemp2>Salaryofemp3){
+    console.log("Employee 2 has highest Salary Among other Employees")
+}else{
+    console.log("Employee 3 has highest Salary Among other Employees")
+}
