af0bf4232869a7009c0968d561fc93a8c19fc637
arifaslam2002 <aarifaslam2002@gmail.com>
2026-06-10 21:02:29 +0530

    Grading

---
+++ grading.js
@@ -0,0 +1,13 @@
+let mark = 62;
+if(100>mark  && mark>90){
+    console.log("Grade-A")
+}
+else if(89>=mark  && mark>=80){
+    console.log("Grade-B")
+}
+else if(79>=mark  && mark>=70){
+    console.log("Grade-c")
+}
+else{
+    console.log("Failed")
+}
