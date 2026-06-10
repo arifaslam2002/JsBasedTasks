afa7adcff050644737001dcdabe0059bc245ce7c
arifaslam2002 <aarifaslam2002@gmail.com>
2026-06-10 21:48:15 +0530

    Middle Number

---
+++ middle.js
@@ -0,0 +1,13 @@
+let a=30;
+let b=20;
+let c=20;
+
+if(a<b && b<c){
+    console.log("B is the Middle Number")
+}
+else if(a>b && a<c){
+    console.log("A is the Middle Number")
+}
+else{
+    console.log("C is the Middle Number")
+}
