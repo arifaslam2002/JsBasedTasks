44a051df8bb4a2e23c5be02429e42d2a66e447b3
arifaslam2002 <aarifaslam2002@gmail.com>
2026-06-10 22:02:07 +0530

    Greatest Number form 3 numbers

---
+++ Greatest.js
@@ -0,0 +1,11 @@
+let a = 1782;
+let b = 785;
+let c = 85;
+
+if (a > b && a > c) {
+  console.log("a is greater among 3 number");
+} else if (b > a && b > c) {
+  console.log("b is greater among 3 number");
+} else {
+  console.log("c is greater among 3 number");
+}
