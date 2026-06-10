8b5de29b3bcc5a22e597f3a6b1415dcc941d3b2b
arifaslam2002 <aarifaslam2002@gmail.com>
2026-06-10 22:13:17 +0530

    Month Based Seasons

---
+++ sesons.js
@@ -0,0 +1,41 @@
+let month = "January";
+
+switch (month) {
+  case "March":
+    console.log("Spring");
+    break;
+  case "April":
+    console.log("Spring");
+    break;
+  case "May":
+    console.log("Spring");
+    break;
+  case "June":
+    console.log("Summer");
+    break;
+  case "July":
+    console.log("Summer");
+    break;
+  case "August":
+    console.log("Summer");
+    break;
+  case "September":
+    console.log("Autumn");
+    break;
+  case "October":
+    console.log("Autumn");
+    break;
+  case "November":
+    console.log("Autumn");
+    break;
+  case "December":
+    console.log("Winter");
+    break;
+  case "January":
+    console.log("Winter");
+    break;
+  default:
+    console.log("Winter");
+    break;
+    break;
+}
