8f85e11f1e98653582d9b31d3615b432ef177afc
arifaslam2002 <aarifaslam2002@gmail.com>
2026-06-10 21:54:06 +0530

    Triangle Form by Angles

---
+++ trainglebyangle.js
@@ -0,0 +1,9 @@
+let angle1=60;
+let angle2=60;
+let angle3=60;
+
+if (angle1+angle2+angle3 == 180) {
+    console.log("These Angles Can form A triangle")
+} else {
+    console.log("These Angles Can't form A triangle")
+}
