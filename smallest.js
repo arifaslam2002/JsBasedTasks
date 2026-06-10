a4df501a156b58299e3dd95578667c313a73e72f
arifaslam2002 <aarifaslam2002@gmail.com>
2026-06-10 19:43:37 +0530

    smallest Number from 3 numbers

---
+++ smallest.js
@@ -0,0 +1,11 @@
+let a=1787852;
+let  b=74585;
+let c=857852;
+
+if(a<b && a<c){
+    console.log("a is smallest among 3 number")
+}else if (b<a && b<c){
+    console.log("b is smallest among 3 number")
+}else{
+    console.log("c is smallest among 3 number")
+}
