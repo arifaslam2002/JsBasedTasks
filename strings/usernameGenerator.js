080e8f32d060b668c917831dd3e514ff30576240
arifaslam2002 <aarifaslam2002@gmail.com>
2026-06-13 09:57:12 +0530

    Username Generator

--- strings/usernameGenerator.js
+++ strings/usernameGenerator.js
@@ -4,4 +4,4 @@ let lastName = "Jordan";
 console.log(firstName.slice(0,3))
 console.log(lastName.slice(0,3))
 let length = firstName.length + lastName.length
-console.log(length)
+console.log(firstName.slice(0,3)+lastName.slice(0,3)+length)
