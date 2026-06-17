063d4281ecf5e3355b336966daedb6c73467a249
arifaslam2002 <aarifaslam2002@gmail.com>
2026-06-17 13:06:29 +0530

    Palindrome Checker

--- Exam/13Palindrome.js
+++ Exam/13Palindrome.js
@@ -1,3 +1,9 @@
 function Palindrome(str){
+    let palindrome=""
     str = str.toLowerCase()
+    for(i=str.length-1;i>=0;i--){
+        palindrome += str[i];
+    }
+    return(palindrome == str)?console.log("yes it is a palindrome"):console.log("yes it is Not a palindrome")
 }
+Palindrome(("madam"))
